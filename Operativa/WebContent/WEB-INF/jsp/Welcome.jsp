2e017f33342ba68b3acb4b9522de72435906b6fe
mpayetta <mpayetta@gmail.com>
2011-03-17 23:29:40 +0000

    Creada entidad PuntoEntrega y menú

--- Operativa/WebContent/WEB-INF/jsp/Welcome.jsp
+++ Operativa/WebContent/WEB-INF/jsp/Welcome.jsp
@@ -1,23 +1,4 @@
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <%@ taglib prefix="s" uri="/struts-tags"%>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-   "http://www.w3.org/TR/html4/loose.dtd">
 
-<html>
-    <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>Bienvenido al proyecto final de Investigación Operativa</title>
-        <meta name="viewport" content="initial-scale=1.0, user-scalable=yes" />
-
-    </head>
-    
-    <body>
-        <p>Bienvenido. Elija una de las opciones para comenzar:</p>
-        
-        <s:form action="welcome.action" method="post">
-        	
-        	<s:submit method="initAbmFabricas" key="label.abmFabricas" align="center" />
-        	
-        </s:form> 
-    </body>
-</html>
+<jsp:include page="menu.jsp" />
--- Operativa/src/operativa/action/FabricasAbmAction.java
+++ Operativa/src/operativa/action/FabricasAbmAction.java
@@ -9,6 +9,47 @@ public class FabricasAbmAction {
 	private String localizacion;
 	private String produccion;
 	private String nombre;
+	
+	public String getLocalizacion() {
+		return localizacion;
+	}
+
+	public void setLocalizacion(String localizacion) {
+		this.localizacion = localizacion;
+	}
+
+	public String getProduccion() {
+		return produccion;
+	}
+
+	public void setProduccion(String produccion) {
+		this.produccion = produccion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getLatitud() {
+		return latitud;
+	}
+
+	public void setLatitud(String latitud) {
+		this.latitud = latitud;
+	}
+
+	public String getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(String longitud) {
+		this.longitud = longitud;
+	}
+
 	private String latitud;
 	private String longitud;
 	
